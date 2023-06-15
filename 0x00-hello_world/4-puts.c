a17b73365d47278678285997fdd3de7fbc0a9657
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-15 10:53:09 -0700

    ADFASDF

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - prints the word given exactly followed by a new line
+ * return 0
+ */
 int main(void)
 {
-     char word[]="\"Programming is like building a multilingual puzzle";
+char word[] = "\"Programming is like building a multilingual puzzle";
 
-     puts(word);
-     
-     return (0);
+puts(word);
+
+return (0);
 }
