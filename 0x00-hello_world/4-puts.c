2e7f5f80b5dfa025456425e3bf2cefde694de13c
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-15 10:43:19 -0700

    ASDFASD

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 int main ()
 {
-     char word[]='"Programming is like building a multilingual puzzle';
+     char word[]="\"Programming is like building a multilingual puzzle";
      puts(word);	 
-     return 0
+     return 0;
 }
