76e09245f9f26c8422a206114b1f01442dc4fccd
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-15 11:01:43 -0700

    ASDF

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
+/**
  * main - it print exact word
  * Returns: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
