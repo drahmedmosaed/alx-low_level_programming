10a223f998a7037813212b790054e2baf408e1cc
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-16 14:56:11 -0700

    QWERW

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - aho yall mashyeen
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	putchar("\n");
+	return (0);
+}
