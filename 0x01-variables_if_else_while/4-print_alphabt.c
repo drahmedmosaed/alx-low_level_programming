4698192e3e796fe3f41821f150c85392feedca9c
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-16 15:28:32 -0700

    WERWE

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - hello world
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while (x <= 'z')
+	{
+		if (x == 'q' || x == 'e')
+		{
+			x++;
+		}
+		else
+		{
+			putchar(x);
+			x++;
+		}
+	}
+	putchar('\n');
+}
