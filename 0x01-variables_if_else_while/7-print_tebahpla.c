809c94ecca3cf1f888bd463e5cbf483fc4f12cdc
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-16 16:17:04 -0700

    WQER

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - aho yall mashyeen
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
