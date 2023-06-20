b059c3cc5908f88e1a36573d11c368347f0675f3
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-20 15:15:13 -0700

    EARES

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * print_alphabet - printing alphabet
  * Return: 0
@@ -11,5 +11,5 @@ void print_alphabet(void)
 	{
 		_putchar(i);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
