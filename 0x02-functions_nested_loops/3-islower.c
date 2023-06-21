8bd55d6221137a6be5295292fbe751e68784b655
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-20 18:41:47 -0700

    dddd

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,13 @@
 #include "main.h"
 /**
- * _islower - aho donya
- * @c: bdan
- * Return: 1-0
- */
+*_islower - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
+*/
 int _islower(int c)
 {
-if ( c >= 'a' && c <= 'z' )
-{
+if (c >= 'a' && c <= 'z')
 return (1);
-else
-{
+
 return (0);
-}
-_putchar('\n');
 }
