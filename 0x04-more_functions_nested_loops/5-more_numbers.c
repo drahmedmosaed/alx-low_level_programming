af7d85f2cd5de48764b00b5373038761744f239d
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-22 15:51:10 -0700

    WQER

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - asdfasdf
+ * Return: asdf
+ */
+void more_numbers(void)
+{
+	int r;
+	for (r = 0 ; r <= 10 ; i++)
+	{
+	int i;
+	for (i = 0 ; i <= 14 ; i++)
+	{
+		_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
+	}
+	_putchar('\n');
+	}
+}
