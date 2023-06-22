a29e921e2166f2045df9b5566a4adb32fed478d9
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-22 16:41:46 -0700

    AER

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - hello
+ * @size : size
+ * Return:wmalo
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			int j;
+
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
