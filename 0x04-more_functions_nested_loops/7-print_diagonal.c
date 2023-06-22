930ae0a62cbf0eebd89154aaa1ba39142e9da6af
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-22 16:27:47 -0700

    ya rab

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - hello
+ * n@: number of numbers
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int s;
+
+	for (s = 0 ; s <= n ; s++)
+	{
+		int r;
+
+		for (r = 0 ; r <= s ; r++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		putchar('\n');
+	}
+}
