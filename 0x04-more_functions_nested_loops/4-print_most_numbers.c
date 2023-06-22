c60accc663355541e2c9cdb9b9b65f78996f1d52
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-22 15:26:03 -0700

    WER

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_most_numbers - donya
+ * Return: bed
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		if (i == 2 || i == 4)
+		{
+			i++
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0'); 
+		}
+	}
+	_putchar('\n');
+}
