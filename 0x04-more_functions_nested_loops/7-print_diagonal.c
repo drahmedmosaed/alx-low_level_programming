899c6b863596576c2f7c2fb9ddf32178bc0a4379
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-22 16:35:03 -0700

    WERWER

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * print_diagonal - hello
- * n@: number of numbers
+ * @n: number of numbers
  * Return: void
  */
 void print_diagonal(int n)
 {
-	if ( n <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
@@ -14,11 +14,11 @@ void print_diagonal(int n)
 	{
 	int s;
 
-	for (s = 0 ; s <= n ; s++)
+	for (s = 0 ; s < n ; s++)
 	{
 		int r;
 
-		for (r = 0 ; r <= s ; r++)
+		for (r = 0 ; r < s ; r++)
 		{
 			_putchar(' ');
 		}
