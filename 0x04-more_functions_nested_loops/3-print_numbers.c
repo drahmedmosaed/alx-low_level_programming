758cd24ac539db077ad5db6cf6452bb29ab94698
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-22 15:08:17 -0700

    QWER

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - aho ay kalam
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		_putchar(i);
+		i++
+	}
+	_putchar('\n');
+}
