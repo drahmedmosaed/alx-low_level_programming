7016dd30565e173e01a57ff67ef6c6651d2aa01e
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-26 14:14:20 -0700

    ya rab

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,19 +7,16 @@
 void print_rev(char *s)
 {
 	int i, o, len;
-	
-	i = 0;
-	
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	
-	len = i;
+i = 0;
 
-	for (o = len - 1 ; o >= 0 ; o--)
-	{
-		_putchar(s[o]);
-	}
+while (s[i] != '\0')
+{
+	i++;
+}
+len = i;
+for (o = len - 1 ; o >= 0 ; o--)
+{
+	_putchar(s[o]);
+}
 	_putchar('\n');
 }
