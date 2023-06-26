1882d91c5f5b40c6113fd79b4d0cfa9d7424cd19
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-26 15:27:59 -0700

    ERW

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,23 +8,23 @@
  */
 void rev_string(char *s)
 {
-    char temp;
-    int i, len, r;
+char temp;
+int i, len, r;
 
-    len = 0;
-    r = 0;
+len = 0;
+r = 0;
 
-    while (s[len] != '\0')
-    {
-        len++;
-    }
+while (s[len] != '\0')
+{
+len++;
+}
 
-    r = len - 1;
+r = len - 1;
 
-    for (i = 0; i < len / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[r];
-        s[r--] = temp;
-    }
+for (i = 0; i < len / 2; i++)
+{
+temp = s[i];
+s[i] = s[r];
+s[r--] = temp;
+}
 }
