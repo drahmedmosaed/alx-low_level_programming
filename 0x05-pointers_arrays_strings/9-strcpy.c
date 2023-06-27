7bc9384ff579dd6d9886a526cdfead397ff12537
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-27 03:23:40 -0700

    awal

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * *_strcpy - bt3weg
+ * @dest: pointer
+ * @src: pointer
+ * Return: beed
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = _strlen(src);
+
+	for (i = 0 ; i <= len ; i++)
+	{
+		src[i] = dest[i];
+	}
+	return (dest)
+}
