d25f36823f54f4fcc2ee95f12368f99ff4876b0f
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-27 02:43:14 -0700

    again

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,13 @@ void puts2(char *str)
 {
 	int len, i;
 
-	for (len = 0 ; s[len] != '\0' ; len++);
+	for (len = 0 ; str[len] != '\0' ; len++)
+		;
 	for (i = 0 ; i <= len ; i++)
 	{
-		if (i % 2 = 0)
+		if (i % 2 == 0)
 		{
-			_putchar(s[i]);
+			_putchar(str[i]);
 		}
 		else
 			continue;
