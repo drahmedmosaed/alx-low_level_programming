2d3d027f04e5479b47cbf23607f63403d658c50f
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-27 02:53:12 -0700

    new year

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * puts_half - print second half
+ * @str: the string
+ * Return: aho mashya
+ */
+void puts_half(char *str)
+{
+	int len, i;
+
+	len = _strlen(str);
+
+	if ( len % 2 == 0 )
+	{
+		for ( i = len / 2 ; i < len ; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for ( i = len - 1 / 2 ; i < len ; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+}
