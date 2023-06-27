53aa66019e9c9d8cf9d8f898aa97eddb643f71b5
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-27 02:39:58 -0700

    no6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - bt2ool
+ * @str: mashy
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	for (len = 0 ; s[len] != '\0' ; len++);
+	for (i = 0 ; i <= len ; i++)
+	{
+		if (i % 2 = 0)
+		{
+			_putchar(s[i]);
+		}
+		else
+			continue;
+	}
+	_putchar('\n');
+}
