3b205825dcfbce2ccfd41c322094561d273229f7
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-29 15:10:21 -0700

    tase3

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *cap_string: capitalize the first char of each word
+ * *cap_string - capitalize the first char of each word
  * @s: the string
  * Return: the string
  */
