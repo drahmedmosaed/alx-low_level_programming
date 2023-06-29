2807baa800575e33b44e7ff221bf223cd4c9838e
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-29 12:23:04 -0700

    two

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,22 +8,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-        int lend, lens;
+	int lend, lens;
 
-        lend = 0;
-        lens = 0;
+lend = 0;
+lens = 0;
 
-        while (dest[lend] != '\0')
-        {
-                lend++;
-        }
-        while (src[lens] != '\0' && lens < n)
-        {
-                dest[lend] = src[lens];
-                lend++;
-                lens++;
-        }
-        dest[lend] = '\0';
+	while (dest[lend] != '\0')
+	{
+		lend++;
+	}
+	while (src[lens] != '\0' && lens < n)
+	{
+		dest[lend] = src[lens];
+		lend++;
+		lens++;
+	}
+	dest[lend] = '\0';
 
-        return (dest);
+	return (dest);
 }
