fb62a43c205f0f964276108dffb678aacc1edd00
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-06-29 12:45:40 -0700

    first

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strncpy - same as *_strncpy
+ * @src: source
+ * @dest: destination
+ * @n: number of bytes
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	while (dest[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
