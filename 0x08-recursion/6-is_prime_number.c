20e0edfd4ebe61cd12172e6e673c580e6ca499b0
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-07-05 15:06:20 -0700

    finally 2

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,13 +10,13 @@ int is_prime_number(int n)
 	if (n <= 1)
 		return (0);
 	else
-		return (prime(n, n-1));
+		return (prime(n, n - 1));
 }
 /**
  * prime - calculcat priminity
- * @n: number 
+ * @n: number
  * @i: iterat
- * Return q or 0
+ * Return: q or 0
  */
 int prime(int n, int i)
 {
