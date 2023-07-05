58ce79b796feb9c9026ccee0d0c0c813d5d0e464
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-07-05 13:13:12 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _pow_recursion - power 
+ * @x: the x
+ * @y: the y
+ * Return: the power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return ( x *  _pow_recursion(x, y - 1));
+}
