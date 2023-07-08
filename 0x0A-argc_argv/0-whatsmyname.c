16191f62e5e11c40fadff5b7902e524434d8d89c
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-07-07 18:09:22 -0700

    one

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program name
+ * @argc: count
+ * @argv: value
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
