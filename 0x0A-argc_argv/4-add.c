92843652e220291fe0ccde337781fd42cfa481b3
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-07-07 19:04:50 -0700

    fourth

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - add +ve only
+ * @argc: count
+ * @argv: value
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
