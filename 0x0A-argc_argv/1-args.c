ed0dc1b8c024961dc6e299cf4fa68d30c3490869
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-07-07 18:18:26 -0700

    de

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,8 +4,9 @@
  * main - number of argument
  * @argc: count
  * @argv: value
+ * Return: 0
  */
-int main(int argc, char  **argv)
+int main(int argc, char  **argvi __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
